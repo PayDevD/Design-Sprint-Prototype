516286cb6782d727a7da17a0a704e99ee85f7f62
PayDevD <mrpaydfordev@gmail.com>
2020-04-04 17:08:27 +0900

    add classes

--- active.js
+++ active.js
@@ -1,6 +1,8 @@
 const applyButton = document.getElementById("applyButton");
 const removeButton = document.getElementById("removeButton");
 const timeTableButton = document.getElementById("timeTableButton");
+let priority = 0;
+let selectedPriority = 0;
 
 timeTableButton.onclick = () => {
   console.log("시간표 보기");
@@ -10,66 +12,42 @@ timeTableButton.onclick = () => {
  시간표 보기와 신청이 일치하는 case를 prototype으로 잡고 
 같은 html문서로 이동하도록 함
 */
-
+let flag = 0;
 applyButton.onclick = () => {
-    console.log("신청");
-    location.href = "result.html";
-}
 
-// 희망 과목 선택 확인용 flag
-let flag1, flag2, flag3, flag4;
-flag1 = flag2 = flag3 = flag4 = 0;
-
-document.getElementById("class1").onclick = () => {
-  if (flag1 === 0) {
-    flag1 = 1;
+  if (flag === 0) {
     // WishList 추가
+    priority++;
     document.getElementById("tblWish").innerHTML +=
-      "<tr>\
-       <td>1</td>\
-       <td>데이터통신</td>\
-       </tr>";
+      `<tr id=${priority}>\
+     <td>${priority}</td>\
+     <td>데이터통신</td>\
+     </tr>`;
     // 학점 추가
     document.getElementById("majorCredit").innerHTML =
       Number(document.getElementById("majorCredit").innerText) + 3;
   }
+  document.getElementById("plan").innerText = "";
+  document.getElementById("evaluation").innerText = "";
 };
 
-document.getElementById("class2").onclick = () => {
-  if (flag2 === 0) {
-    flag2 = 1;
-    document.getElementById("tblWish").innerHTML +=
-      "<tr>\
-        <td>2</td>\
-        <td>운영체제및실습</td>\
-        </tr>";
-    document.getElementById("majorCredit").innerHTML =
-      Number(document.getElementById("majorCredit").innerText) + 3;
+// 희망 과목 선택 확인용 flag
+let flag1, flag2, flag3, flag4, flag5, flag6, flag7, flag8, flag9, flag10, flag11, flag12, flag13, flag14;
+flag1 = flag2 = flag3 = flag4 = flag5 = flag6 = flag7 = flag8 = flag9 = flag10 = flag11 = falg12 = flag13 = flag14 = 0;
+
+document.getElementById("class5").onclick = () => {
+    document.getElementById("plan").innerText = 
+    "수업목표 : Oracle SQL 11g2 를 사용하여 SQL문을 익히고, ER 모델을 활용하여 계층형 데이터베이스를 설계할 수 있다.";
+    document.getElementById("evaluation").innerText = 
+    "평가 :  출석 10 / 과제 30 / 중간고사(지필평가) 30 / 기말고사(프로젝트) 30";
+    flag5 = 1;
   }
 };
 
-document.getElementById("class3").onclick = () => {
-  if (flag3 === 0) {
-    flag3 = 1;
-    document.getElementById("tblWish").innerHTML +=
-      "<tr>\
-        <td>3</td>\
-        <td>웹프로그래밍</td>\
-        </tr>";
-    document.getElementById("majorCredit").innerHTML =
-      Number(document.getElementById("majorCredit").innerText) + 3;
-  }
-};
+document.getElementById("class1").onclick = () => {
 
-document.getElementById("class4").onclick = () => {
-  if (flag4 === 0) {
-    flag4 = 1;
-    document.getElementById("tblWish").innerHTML +=
-      "<tr>\
-        <td>4</td>\
-        <td>프로그래밍언어개론</td>\
-        </tr>";
-    document.getElementById("majorCredit").innerHTML =
-      Number(document.getElementById("majorCredit").innerText) + 3;
-  }
-};
+}
+
+document.getElementById("upButton").onlick = () => {
+  
+}
